0021e3bd25494cab158b43322b29668a6ccde2dc
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-12-11 14:00:54 +0600

    Add functionality to select and mute

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ class DrumKit{
     constructor() {
         this.pads = document.querySelectorAll('.pad');
         this.playBtn = document.querySelector('.play');
+        this.currentKick = './sounds/kick-classic.wav';
+        this.currentSnare = './sounds/snare-acoustic01.wav';
+        this.currentHihat = './sounds/hihat-acoustic01.wav';
         this.kickAudio = document.querySelector('.kick-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
         this.bpm = 150;
         this.isPlaying = null;
+        this.selects = document.querySelectorAll('select');
+        this.muteBtns = document.querySelectorAll('.mute');
     }
     activePad() {
         this.classList.toggle('active');
@@ -38,16 +43,76 @@ class DrumKit{
         this.index++;
     }
     start() {
-        console.log(this);
         const interval = (60 / this.bpm) * 1000;
-        setInterval(() => {
+        //Check if its playing
+        if(!this.isPlaying) {
+          this.isPlaying = setInterval(() => {
             this.repeat();
-        }, interval)
+      }, interval)
+    } else {
+        //Clear the interval
+        clearInterval(this.isPlaying)
+        this.isPlaying = null;
     }
+  }
+  updateBtn() {
+    if(!this.isPlaying) {
+        this.playBtn.innerText = "Stop";
+        this.playBtn.classList.add('active');
+    }else {
+        this.playBtn.innerText = "Play";
+        this.playBtn.classList.remove('active');
+    }
+  }
+  changeSound(e) {
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+    switch(selectionName) {
+        case "kick-select":
+            this.kickAudio.src = selectionValue;
+            break;
+        case "snare-select":
+            this.snareAudio.src = selectionValue;
+            break;
+        case "hihat-select":
+            this.hihatAudio.src = selectionValue;
+            break;
+    }
+  }
+  mute(e) {
+    const muteIndex = e.target.getAttribute('data-track');
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+        switch (muteIndex) {
+            case '0':
+                this.kickAudio.volume = 0;
+                break;
+            case '1':
+                this.snareAudio.volume = 0;
+                break;
+            case '2':
+                this.hihatAudio.volume = 0;
+                break;
+        }
+    } else {
+        switch(muteIndex) {
+            case '0':
+                this.kickAudio.volume = 1;
+                break;
+            case '1':
+                this.snareAudio.volume = 1;
+                break;
+            case '2':
+                this.hihatAudio.volume = 1;
+                break;
+        }
+    }
+  }
 }
 
 const drumKit = new DrumKit();
 
+//EVENT LISTENERS
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad);
     pad.addEventListener('animationend', function() {
@@ -56,5 +121,18 @@ drumKit.pads.forEach(pad => {
 })
 
 drumKit.playBtn.addEventListener('click', function() {
+    drumKit.updateBtn();
     drumKit.start();
+})
+
+drumKit.selects.forEach(select => {
+    select.addEventListener('change', function(e) {
+        drumKit.changeSound(e);
+    })
+})
+
+drumKit.muteBtns.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        drumKit.mute(e);
+    })
 })
