02c3239218852a3ebd727121d623eadd1d67e379
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-12-11 11:24:16 +0600

    Add sounds

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ class DrumKit{
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
         this.bpm = 150;
+        this.isPlaying = null;
     }
     activePad() {
         this.classList.toggle('active');
@@ -14,11 +15,31 @@ class DrumKit{
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
-        console.log(step);
+        //Loop over the pads
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`
+            //Check if pads are active
+            if(bar.classList.contains('active')) {
+                //Check each sound
+                if(bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
     }
     start() {
-        const interval = (60/this.bpm) * 1000;
+        console.log(this);
+        const interval = (60 / this.bpm) * 1000;
         setInterval(() => {
             this.repeat();
         }, interval)
@@ -28,9 +49,12 @@ class DrumKit{
 const drumKit = new DrumKit();
 
 drumKit.pads.forEach(pad => {
-    pad.addEventListener('click', drumKit.activePad)
+    pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function() {
+        this.style.animation ="";
+    })
 })
 
-drumKit.playBtn.addEventListener('click', () => {
+drumKit.playBtn.addEventListener('click', function() {
     drumKit.start();
 })
